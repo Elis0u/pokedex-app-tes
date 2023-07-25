148c86ecb3463e3db6f52f4fdc6f6888964d4180
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-07-25 20:41:26 +0200

    UPDATE onSubmit method

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -15,11 +15,13 @@ export class PokemonFormComponent implements OnInit {
 
   @Input() pokemon: Pokemon;
   types: string[];
+  isAddForm: boolean;
 
   constructor(private pokemonService: PokemonService, private router: Router) { }
 
   ngOnInit() {
     this.types = this.pokemonService.getPokemonTypeList();
+    this.isAddForm = this.router.url.includes('add');
   }
 
   hasType(type: string): boolean {
@@ -51,7 +53,11 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.pokemonService.updatePokemon(this.pokemon).subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    if(this.isAddForm){
+      this.pokemonService.addPokemon(this.pokemon).subscribe((pokemon: Pokemon) => this.router.navigate(['/pokemon', pokemon.id]) )
+    } else {
+      this.pokemonService.updatePokemon(this.pokemon).subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    }
   }
 
 }
