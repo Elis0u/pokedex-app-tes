8a733b16e7025e4d802630e52acb8296aad19a5a
Elis0u <42451634+Elis0u@users.noreply.github.com>
2023-07-27 00:39:51 +0200

    CREATE search & goToDetail method

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-pokemon',
   templateUrl: './search-pokemon.component.html',
-  styles: [
-  ]
 })
-export class SearchPokemonComponent {
+export class SearchPokemonComponent implements OnInit {
+  // {...'a'..'ab..abs...ab...abc........}
+  searchTerm = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() { }
+
+  search(term: string){
+    this.searchTerm.next(term);
+  }
+
+  goToDetail(pokemon: Pokemon){
+    const link = ['/pokemon', pokemon.id];
+    this.router.navigate(link);
+  }
 
 }
